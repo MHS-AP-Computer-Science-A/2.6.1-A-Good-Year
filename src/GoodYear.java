53ffc5e13e4518f08eb8b6655e17967c918c3450
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2021-09-22 12:17:23 -0400

    Add files via upload

---
+++ src/GoodYear.java
@@ -0,0 +1,19 @@
+
+public class GoodYear {
+	public static void main(String[] args) {
+		for(int year = 1900; year <= 2500; year++) {
+			//Convert year to a string
+			String yearString = 
+			
+			//Extact the first 2, last 2, and middle 2 digits
+	
+			
+			//Convert the extracted Strings to numbers
+
+			
+			//Check to see if the year if good and println
+		
+			
+		}
+	}
+}
